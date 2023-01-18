801d1e2d06a95a2a4a15bd5125c84fd7d0532429
Gautham <gauthaman101@gmail.com>
2023-01-18 18:07:56 +0530

    first

---
+++ quote.js
@@ -0,0 +1,17 @@
+let btnFun = document.getElementById('btnFunc')
+
+
+btnFun.addEventListener('click',()=>{
+  fetch("https://api.themotivate365.com/stoic-quote")
+  .then(ip=>ip.json())
+  .then((data)=>{
+    document.getElementById("quote").innerHTML = data.quote;
+    document.getElementById("author").innerHTML =data.author;
+   
+  
+  })
+  
+
+})
+
+
